a3b47f1dc72fc196a122af069dc0cfc74eba2361
Lucas <luc.hernandes@gmail.com>
2020-10-22 20:39:51 -0300

    Button changes activity

--- mobile/app/src/main/java/com/projetosuniso/digdin/LoginActivity.java
+++ mobile/app/src/main/java/com/projetosuniso/digdin/LoginActivity.java
@@ -3,13 +3,30 @@ package com.projetosuniso.digdin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.entrarButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMenu();
+            }
+        });
+    }
+
+    public void openMenu() {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
--- mobile/app/src/main/java/com/projetosuniso/digdin/MenuActivity.java
+++ mobile/app/src/main/java/com/projetosuniso/digdin/MenuActivity.java
@@ -3,14 +3,31 @@ package com.projetosuniso.digdin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends Activity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        button = (Button) findViewById(R.id.sideMenuButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+    }
+
+    public void openLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 }
