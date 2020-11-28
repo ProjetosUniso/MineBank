c9ccad2d203fadaad62502dabfe785bb15f55186
Lucas Candido Hernandes <luc.hernandes@gmail.com>
2020-11-28 14:10:06 -0300

    Ajustes na tela de login + warnings

--- mobile/app/src/main/java/com/projetosuniso/digdin/LoginActivity.java
+++ mobile/app/src/main/java/com/projetosuniso/digdin/LoginActivity.java
@@ -1,12 +1,18 @@
 package com.projetosuniso.digdin;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import com.projetosuniso.digdin.model.Conta;
 import com.projetosuniso.digdin.service.ContaService;
 import com.projetosuniso.digdin.service.EnderecoService;
@@ -26,8 +32,77 @@ public class LoginActivity extends Activity {
 
         mediaPlayer = MediaPlayer.create(this, R.raw.button_click);
 
-        EditText editTextCPF = findViewById(R.id.editTextCPF);
+        final EditText editTextSenha = findViewById(R.id.editTextSENHA);
+
+        final EditText editTextCPF = findViewById(R.id.editTextCPF);
+
+        final TextView textViewInvalido = findViewById(R.id.textLoginInvalido);
+
+        editTextCPF.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    textViewInvalido.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
         editTextCPF.addTextChangedListener(MaskEditUtil.mask(editTextCPF, MaskEditUtil.FORMAT_CPF));
+        editTextCPF.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                String cpfLenght = editTextCPF.getText().toString();
+
+                if (cpfLenght.length() == 14) {
+                    editTextCPF.clearFocus();
+                    editTextSenha.requestFocus();
+                }
+            }
+        });
+
+        editTextSenha.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    textViewInvalido.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        editTextSenha.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String senha = editTextSenha.getText().toString();
+
+                if(senha.length() == 6) {
+                    InputMethodManager imm =  (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(editTextSenha.getWindowToken(), 0);
+                    editTextSenha.clearFocus();
+                }
+
+            }
+        });
     }
 
     /*Chama o metodo Login para fazer a validação do cpf e senha indicado pelo cliente*/
@@ -44,6 +119,11 @@ public class LoginActivity extends Activity {
         if (loginExiste) {
             Intent intent = new Intent(this, MenuActivity.class);
             startActivity(intent);
+        }
+        else {
+            TextView textViewInvalido = findViewById(R.id.textLoginInvalido);
+
+            textViewInvalido.setVisibility(View.VISIBLE);
         }
     }
 
